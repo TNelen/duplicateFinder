7aafb0a107a7dfbeff3ed01c79981d736325dc17
Timo <timo.nelen@student.uantwerpen.be>
2020-03-06 15:48:37 +0100

    run thorugh subfolders

--- fileIterator.py
+++ fileIterator.py
@@ -1,19 +1,56 @@
 import os
 
-path = 'E:\\Music\\FAVS'
+path = 'E:\\Music'
+
+def Diff(li1, li2):
+    difference = []
+    for i in li1:
+        if i not in li2:
+            difference.append(i)
+        else:
+            li2.remove(i)
+    return difference
 
 def main():
-    files = []
+    songs = []
+    songname = []
     # r=root, d=directories, f = files
     for r, d, f in os.walk(path):
-        for file in f:
-            if '.mp3' in file:
-                files.append((file))
+        #print(d)
+        print(len(songs))
+        for dirs in d:
+            pathname =r + "\\"+ dirs
+            print(pathname)
+            for r2, d2, f2 in os.walk(pathname):
+                for file in f2:
+                    if '.mp3' in file:
+                        if ' - ' in file:
+                            songname = file.split(" - ")
+                            title = songname[1]
+                        else:
+                            title = file
 
-    for f in files:
+                        if title.endswith('.mp3'):
+                            title = title[:-4]
+                        songs.append(title)
+
+
+
+    for f in songs:
         print(f)
+    print("total number of songs")
+    print(len(songs))
     
+    uniques = set(songs)
+    print("number of unique songs")
+    print (len(uniques))
 
+    duplicates = Diff(songs, uniques) 
+    print("number of duplicates")  
+    print (len(duplicates))
+    print("duplicates:")
+    #for f in duplicates:
+        #print(f)
 
 
 if __name__ == "__main__":
